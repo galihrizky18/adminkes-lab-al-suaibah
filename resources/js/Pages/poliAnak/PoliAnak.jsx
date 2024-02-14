8ea270ef875e7ffaad568a6ad95fc47cdd90ad7c
rifvoliverorlando31 <rifvogit@gmail.com>
2024-02-14 16:07:56 +0700

    revisi responsive

--- resources/js/Pages/poliAnak/PoliAnak.jsx
+++ resources/js/Pages/poliAnak/PoliAnak.jsx
@@ -26,7 +26,7 @@ const PoliAnak = () => {
             <Navbar />
 
             {/* Body */}
-            <div className=" w-full h-full flex flex-col gap-5 justify-center items-center p-5 sm:p-10 ">
+            <div className=" w-full h-full flex flex-col gap-5 justify-center items-center p-5 sm:p-10">
                 {/* Title */}
                 <div className="w-full sm:w-[50%] bg-white px-5 py-3 text-center font-bold text-2xl font-roboto text-[#237939] rounded-xl shadow-lg ">
                     Ruangan Umum Anak
@@ -42,121 +42,149 @@ const PoliAnak = () => {
                     </div>
                 </div>
 
+                <div>
+                    <h2 className="w-[350px] h-[35px] mx-auto mb-14 border-b-2 border-blue-200 text-center text-[18px] sm md:text-lg lg:text-xl xl:text-2x1">Layanan RU Anak Stikes Al-Suaibah</h2>
+                </div>
+
                 {/* Layanan */}
-               <div className="w-full h-full p-4 flex flex-col mx-aouto  md:px-8 lg:px-12 xl:px-16">
-                    <div>
-                        <h2 className="w-[350px] h-[35px] mx-auto mb-14 border-b-2 border-blue-200 text-center text-[18px] sm md:text-lg lg:text-xl xl:text-2x1">Layanan RU Anak Stikes Al-Suaibah</h2>
-                    </div>
-                    <div className="flex flex-row gap-12 ml-8 mx-aouto sm:px-4 md:px-8 lg:px-12 xl:px-16">
-                        <div className="flex flex-col w-[160px] h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200 ">
-                            <div className="flex justify-center items-center p-6">
-                                <img src={Gambar5} alt="" width={100}/>
-                            </div>
-                            <div className="">
-                                <h2 className="text-[16px] text-center font-source sans pro">Konsultasi Gizi Anak</h2>
-                            </div>
+               
+                <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5 justify-center">
+                    <div class="flex flex-col h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
+                        <div class="flex justify-center items-center p-6">
+                            <img src={Gambar5} alt="" class="sm:w-[100px] sm:h-[100px] xl:w-[100px] xl:h-[100px]" />
                         </div>
-                        <div className="flex flex-col w-[160px] h-[230px]  bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
-                            <div className="flex justify-center items-center p-9">
-                                <img src={Gambar6} alt="" width={50}/>
-                            </div>
-                            <div className="">
-                                <h2 className="text-[16px] text-center font-source sans pro">Konsultasi Tumbuh Kembang Anak</h2>
-                            </div>
-                        </div>
-                        <div className="flex flex-col w-[160px] h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
-                            <div className="flex justify-center items-center p-6">
-                                <img src={Gambar7} alt="" width={100}/>
-                            </div>
-                            <div className="">
-                                <h2 class="text-[16px] text-center font-source sans pro">Medical Checkup Untuk Anak</h2>
-                            </div>
-                        </div>
-                        <div className="flex flex-col w-[160px] h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
-                            <div className="flex justify-center items-center p-6">
-                                <img src={Gambar8} alt="" width={100}/>
-                            </div>
-                            <div className="">
-                                <h2 className="text-[16px] text-center font-source sans pro">Imunisasi</h2>
-                            </div>
-                        </div>
-                        <div className="flex flex-col w-[160px] h-[230px] shadow-md rounded-xl bg-blue-200 hover:scale-110 transition-all duration-200">
-                            <div className="flex justify-center items-center p-6">
-                                <img src={Gambar9} alt="" width={100} />
-                            </div>
-                            <div>
-                                <h2 className="text-[16px] text-center font-source sans pro">Skrining Obesitas</h2>
-                            </div>
+                        <div>
+                            <h2 class="xl:text-[16px] sm:text-[10px] text-center font-source sans pro">Konsultasi Gizi Anak</h2>
                         </div>
                     </div>
-               </div>
+                    <div class="flex flex-col h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
+                        <div class="flex justify-center items-center p-6">
+                            <img src={Gambar6} alt="" class="sm:w-[100px] sm:h-[100px] xl:w-[100px] xl:h-[100px]" />
+                        </div>
+                        <div>
+                            <h2 class="xl:text-[16px] sm:text-[10px] text-center font-source sans pro">Konsultasi Gizi Anak</h2>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
+                        <div class="flex justify-center items-center p-6">
+                            <img src={Gambar7} alt="" class="sm:w-[100px] sm:h-[100px] xl:w-[100px] xl:h-[100px]" />
+                        </div>
+                        <div>
+                            <h2 class="xl:text-[16px] sm:text-[10px] text-center font-source sans pro">Konsultasi Gizi Anak</h2>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200">
+                        <div class="flex justify-center items-center p-6">
+                            <img src={Gambar8} alt="" class="sm:w-[100px] sm:h-[100px] xl:w-[100px] xl:h-[100px]" />
+                        </div>
+                        <div>
+                            <h2 class="xl:text-[16px] sm:text-[10px] text-center font-source sans pro">Konsultasi Gizi Anak</h2>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-[230px] bg-blue-200 rounded-xl shadow-md hover:scale-105 transition-all duration-200 sm:items-center">
+                        <div class="flex justify-center items-center p-6">
+                            <img src={Gambar9} alt="" class="sm:w-[100px] sm:h-[100px] xl:w-[100px] xl:h-[100px]" />
+                        </div>
+                        <div>
+                            <h2 class="xl:text-[16px] sm:text-[10px] text-center font-source sans pro">Konsultasi Gizi Anak</h2>
+                        </div>
+                    </div>
+                </div>
+               
+
 
                 {/* prosedur dan biaya */}
-               <div className="w-full h-[60px] flex flex-col bg-yellow-300 mt-14">
-                    <div className="border-b-4 border-blue-200 w-full flex justify-center items-center ">
+               <div className="w-full h-[60px] flex flex-col bg-yellow-300 mt-[9px]">
+                    <div className=" flex justify-center items-center ">
                         <h2 className="text-[28px] mb-10 mt-2 ">Prosedur</h2>
                     </div>
                </div>
-               <div className="w-full h-[430px] flex flex-col mt-18 justify-center items-center hover:scale-105 transition all duration-200">  
-                   <div className="flex flex-row border-2 border-black mt-12">
-                       <div className="w-[362px] h-[380px] bg-green-400 ">
-                            <div className="border-b-2 border-blue flex flex-row justify-center items-center">
-                                <img src={logoprosedur} alt="" className="w-[52px] h-[45px]"/>
-                                <h2 className="text-[32px] mt-2 font-source sans pro">Prosedur</h2>
-                            </div>
-                            <div className="">
-                                <h2 className="text-[19px] mt-6 font-source sans pro ml-10">1.Pedaftaran pasien baru</h2>                      
-                            </div>
-                            <div className="">
-                                <h2 className="text-[19px] mt-2 font-source sans pro ml-10">2.Penerimaan di ruang tunggu</h2>                      
-                            </div>
-                            <div className="">
-                                <h2 className="text-[19px] mt-2 font-source sans pro ml-10">3.Pemeriksaan</h2>                      
-                            </div>
-                            <div className="">
-                                <h2 className="text-[19px] mt-2 font-source sans pro ml-10">4.Penjelasan dan edukasi</h2>                      
-                            </div>
-                            <div className="">
-                                <h2 className="text-[19px] mt-2 font-source sans pro ml-10">5.Laporan hasil pemeriksaan</h2>                      
-                            </div>
-                            <div className="">
-                                <h2 className="text-[19px] mt-2 font-source sans pro ml-10">6.Pemberian resep obat</h2>                      
-                            </div>
-                        </div>
-                        <div className="w-[320px] h-[380px]">
-                           <img src={fotodokter} alt="" className="h-[380px] w-[320px]" />
-                        </div> 
-                     </div>
-                     {/* <div className="flex flex-row"> 
-                         <div className=" ">
-                            <img src={fotodokter2} alt="" className="h-[380px] w-[320px]" />
-                        </div>
-                        <div className="w-[362px] h-[380px] bg-green-400">
-                            <div className="border-b-2 border-blue flex flex-row justify-center items-center">
-                                <img src={logoprosedur} alt="" className="w-[62px] h-[55px]" />
-                                <h2 className="text-[32px] mt-2 font-source sans pro ">Biaya</h2>
+
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-2 hover:scale-105 transition-all duration-200 mb-10 xl:h-[400px]">
+                    <div className=" xl:h-[300px] xl:w-[380px] sm:h-[50px] sm:w-[50px] ">
+                        <img src={fotodokter} alt="" className="xl:h-[400px] xl:w-[380px] sm:w-[80px] sm:h-[20px]"/>
+                    </div>
+                    <div className="bg-green-700 xl:h-[400px] xl:w-[380px] text-white">
+                        <div class="border-b-2 border-blue flex flex-row justify-center items-center">
+                                <h2 class="text-[32px] mt-2 font-source sans pro">Prosedur</h2>
                             </div>
                             <div>
-                                <h2 className="text-[22px] mt-2 font-source sans pro ml-8">Biaya ditentukan dari tindakan yang diambil dan sesuai ketentuan</h2>
+                                <h2 class="text-[19px] mt-6 font-source sans pro ml-10">1.Pendaftaran pasien baru</h2>
                             </div>
-                        </div>
-                     </div> */}
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">2.Penerimaan di ruang tunggu</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">3.Pemeriksaan</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">4.Penjelasan dan edukasi</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">5.Laporan hasil pemeriksaan</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">6.Pemberian resep obat</h2>
+                            </div>
+                    </div>
                 </div>
-                {/* <div className="w-full h-[60px] flex flex-col bg-yellow-300  mt-18 ">
 
+                {/* <div class="w-full h-[430px] mt-18">  
+                    <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-2 hover:scale-105 transition-all duration-200 justify-center border border-black">
+                        <div class="w-full sm:w-[320px] h-[380px] border border-red-600 flex justify-center items-center">
+                            <img src={fotodokter} alt="" class="xl:h-[380px] xl:w-[320px] sm:w-[50px] sm:h-[50px]" />
+                        </div> 
+                        <div class="w-full sm:w-[70px] sm:h-[100px] xl:w-[200px] xl:h-[380px] bg-green-400 border border-black flex flex-col ">
+                            <div class="border-b-2 border-blue flex flex-row justify-center items-center">
+                                <img src={logoprosedur} alt="" class="xl:w-[52px] xl:h-[45px] sm:w-[10px] sm:h-[15px]" />
+                                <h2 class="text-[32px] mt-2 font-source sans pro">Prosedur</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-6 font-source sans pro ml-10">1.Pendaftaran pasien baru</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">2.Penerimaan di ruang tunggu</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">3.Pemeriksaan</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">4.Penjelasan dan edukasi</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">5.Laporan hasil pemeriksaan</h2>
+                            </div>
+                            <div>
+                                <h2 class="text-[19px] mt-2 font-source sans pro ml-10">6.Pemberian resep obat</h2>
+                            </div>
+                        </div>     
+                    </div>
                 </div> */}
 
+
+
                {/* About */}
-               <div className="w-full h-full bg-gray-300 p-6 rounded-xl shadow-md mt-16">
-                    <h2 className="text-[32px] font-bold mb-4 text-center"> Tentang Kami</h2>
-                    <p className="text-[16px] leading-relaxed">
+               <div className="w-full h-full bg-gray-300 rounded-xl shadow-xl py-2 sm:mt-[100px] xl:mt-[0px]">
+                    <div className="sm:text-center xl:text-center font-bold">
+                        <span>Tentang Kami</span>
+                    </div>
+                    <div className="clas">
+                        <span> Ruang Umum Anak adalah fasilitas kesehatan pada klinik As-Suaibah yang khusus menyediakan layanan medis bagi anak-anak, mulai dari dari bayi hingga remaja. RU Anak ini difokuskan 
+                               untuk pelayanan kesehatan dan pengembangan anak yang melibatkan dokter spesialis anak. 
+                               Layanan yang disediakan meliputi pemeriksaan kesehatan, imunisasi, pemantauan gizi anak, dan lain lain.
+                        </span>
+                    </div>
+               </div>
+               {/* <div className="w-full h-full bg-gray-300 rounded-xl shadow-md xl:py-[20px] sm:mt-[800px] xl:mt-[20px] border border-black">
+                    <h2 className="text-[32px] font-bold  text-center"> Tentang Kami</h2>
+                    <p className="text-[16px]">
                         Ruang Umum Anak adalah fasilitas kesehatan pada klinik As-Suaibah yang khusus menyediakan layanan medis bagi anak-anak, mulai dari dari bayi hingga remaja. RU Anak ini difokuskan untuk pelayanan kesehatan dan pengembangan anak yang melibatkan dokter spesialis anak. Layanan yang disediakan meliputi pemeriksaan kesehatan, imunisasi, pemantauan gizi anak, dan lain lain.
                     </p>
-               </div>
+               </div> */}
             </div>
 
             {/* Blank */}
-            <div className="h-[50px] bg-[#F5F7FA]"></div>
+            <div className="h-[10px] bg-[#F5F7FA]"></div>
 
             {/* Footer */}
             <Footer />
